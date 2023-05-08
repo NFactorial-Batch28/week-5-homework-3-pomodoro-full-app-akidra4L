467d1e877148a5dff59d6d4f1cd690a8a286c11b
Alikhan <jonasofan.gubaev@gmail.com>
2023-05-08 12:23:05 +0600

    initial commit

---
+++ PomodoroFullApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PomodoroFullApp
+//
+//  Created by Alikhan Gubayev on 08.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
